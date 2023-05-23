4cd6e06733162690b6adce727d7000c834acb5df
Ilya Lyalin <lyalin315@gmail.com>
2023-05-23 14:02:48 +0300

    Добавил решение для четвертой задачи

--- hwsem1/zadacha4/Program4.cs
+++ hwsem1/zadacha4/Program4.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int i = 2;
+while(i<a)
+{
+    Console.Write(i+" ");
+    i=i+2;
+}
