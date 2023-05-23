d64d87602ad19a581a9f94eeb01ff0aa0d081602
Ilya Lyalin <lyalin315@gmail.com>
2023-05-23 13:56:03 +0300

    Добавил решение третьей задачи

--- hwsem1/zadacha3/Program3.cs
+++ hwsem1/zadacha3/Program3.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a%2 == 0)
+{
+    Console.Write("Число является чётным");
+}
+else
+{
+    Console.Write("Число является нечётным");
+}
