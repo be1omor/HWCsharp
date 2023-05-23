5206f7b22c57e6ed485f708ac1af2e157cf71bcc
Ilya Lyalin <lyalin315@gmail.com>
2023-05-23 13:49:58 +0300

    Добавил решение второй задачи

--- hwsem1/zadacha2/Program2.cs
+++ hwsem1/zadacha2/Program2.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите три числа:");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int max = 0;
+if (a>max)
+{
+    max = a;
+}
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+Console.WriteLine("Максимальное число = "+max);
