e361fcd8e730b58e6ad2578cbfd2e09282b6a711
Ilya Lyalin <lyalin315@gmail.com>
2023-05-28 17:49:59 +0300

    Добавил решение для третьей задачи(сем.2)

---
+++ hwsem2/zadacha3/Program.cs
@@ -0,0 +1,18 @@
+﻿void FindWeekEnd(int num)
+{
+    if (8 > num && num > 5)
+    {
+        Console.WriteLine("Это выходной день.");
+    }
+    else if (6 > num && num > 0)
+    {
+        Console.WriteLine("Это рабочий день.");
+    }
+    else
+    {
+        Console.WriteLine("Такого дня недели нет :(");
+    }
+}
+Console.Write("Введите число обозначающее день недели: ");
+int number = int.Parse(Console.ReadLine()!);
+FindWeekEnd(number);
