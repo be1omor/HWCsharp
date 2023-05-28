16fa27c46d57bcb47bf699917d85837ff8113a1a
Ilya Lyalin <lyalin315@gmail.com>
2023-05-28 15:03:28 +0300

    Добавил решение для первой задачи(сем.2)

---
+++ hwsem2/zadacha1/Program.cs
@@ -0,0 +1,22 @@
+﻿int FindSecondDigit(int num)
+{
+    int secondNumber = num % 100 / 10;
+    Console.WriteLine("Вторая цифра вашего числа " + secondNumber);
+    return secondNumber;
+}
+
+void Nachalo(int num)
+{
+    if (1000 > num && 99 < num)
+    {
+        FindSecondDigit(num);
+    }
+    else
+    {
+        Console.Write("Вы ввели некорректное число!");
+    }
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Nachalo(number);
