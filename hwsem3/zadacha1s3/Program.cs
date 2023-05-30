25da8fa7b3ee6f88aa7cad7dc5717a7cee8f3843
Ilya Lyalin <lyalin315@gmail.com>
2023-05-30 16:52:06 +0300

    Добавил решение для первой задачи(сем.3)

---
+++ hwsem3/zadacha1s3/Program.cs
@@ -0,0 +1,18 @@
+﻿int FindPal(int number)
+{
+    int num1 = number / 10000 % 10;
+    int num2 = number / 1000 % 10;
+    int num4 = number / 10 % 10;
+    int num5 = number % 10;
+    if (num1 == num5 || num2 == num4) Console.WriteLine("Число является палиндромом");
+    else Console.WriteLine("Число не является палиндромом");
+    return number;
+}
+void Nachalo(int number2)
+{
+    if (number2 > 10000 && number2 < 99999) FindPal(number2);
+    else Console.WriteLine("Введите корректрое число");
+}
+Console.Write("Введите пятизначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+Nachalo(a);
