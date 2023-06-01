3ff5c25d805efbb3d1abf6e9a2ee090e68a83e45
Ilya Lyalin <lyalin315@gmail.com>
2023-06-01 22:12:08 +0300

    Добавил решение для второй и третьей задачи(сем.3)

---
+++ hwsem3/zadacha2s3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int Promtp(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void Distance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double result = Math.Sqrt((Math.Pow((x2 - x1), 2) +
+                                (Math.Pow((y2 - y1), 2)) +
+                                (Math.Pow((z2 - z1), 2))));
+    Console.Write($"Расстояние между точками составляет -> {result}");
+}
+
+int x1 = Promtp("Введите координату х1: ");
+int y1 = Promtp("Введите координату y1: ");
+int z1 = Promtp("Введите координату z1: ");
+int x2 = Promtp("Введите координату x2: ");
+int y2 = Promtp("Введите координату y2: ");
+int z2 = Promtp("Введите координату z2: ");
+Console.WriteLine($"Координата точки А ({x1}, {y1}, {z1}) и точки В ({x2}, {y2}, {z2}) ");
+
+Distance(x1,y1,z1,x2,y2,z2);
---
+++ hwsem3/zadacha3s3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+void Kub(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        double kub=0;
+        kub = Math.Pow((i), 3);
+        Console.Write($"{kub} ");
+    }
+    // int i=1;
+    // while(i<=num)
+    // {
+    //     double kub=0;
+    //     kub = Math.Pow((i), 3);
+    //     Console.Write($"{kub} ");
+    //     i++;
+    // }
+}
+string mess = "Введите число: ";
+int chislo = Prompt(mess);
+Kub(chislo);
