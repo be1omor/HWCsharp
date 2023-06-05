ce14782d6513aafd670c13104fafac66f9c5689a
Ilya Lyalin <lyalin315@gmail.com>
2023-06-05 10:16:18 +0300

    Добавил решение для первой задачи(сем.4)

---
+++ hwsem4/zadacha1s4/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+//int
+void Result(int a, int b)
+{
+    //double result = Math.Pow((a), b);
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+    Console.WriteLine($"{result} ");
+    //return result;
+}
+void Cycle()
+{
+    while (true)
+    {
+        string messA = "Введите число A: ";
+        int numberA = Prompt(messA);
+        string messB = "Введите число B: ";
+        int numberB = Prompt(messB);
+        Result(numberA, numberB);
+    }
+}
+Cycle();
