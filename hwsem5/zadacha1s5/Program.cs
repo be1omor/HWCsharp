df3d61f83c5db3377c338ba8b55fe2cb2ed4e197
Ilya Lyalin <lyalin315@gmail.com>
2023-06-08 19:42:46 +0300

    решение 1й задачи (сем.5)

---
+++ hwsem5/zadacha1s5/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 34: Задайте массив заполненный 
+// случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+Console.Clear();
+int [] Massive (int size) //создаёт массив из трёхзначный 
+                          //чисел заданной длинны
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(100, 1000);
+        //Console.WriteLine(res[i]);
+    }
+    return res;
+}
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+void OutputDisplay(int [] array) //выводит созданный массив на экран
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        Console.WriteLine(array[i] + " ");
+    }
+    Console.WriteLine($"{array[array.Length -1]}");
+}
+void EvenNumbers(int [] num) //считает кол-во чётных элементов массива
+{
+    int i=0;
+    foreach (int el in num)
+    {
+        if (el%2 == 0)
+        {
+            i++;
+        }
+    }
+    Console.WriteLine(i);
+}
+string mess = "Задайте длину массива: ";
+int sizeNumber = Prompt(mess);
+Massive(sizeNumber);
+int [] massiv = Massive(sizeNumber);
+Console.WriteLine("Ваш массив.");
+OutputDisplay(massiv);
+Console.WriteLine("Чётные числа массива: ");
+EvenNumbers(massiv);
