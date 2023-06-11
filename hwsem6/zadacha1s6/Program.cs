ec4152f20414ca3afbb36c72e5025cc380636fa5
Ilya Lyalin <lyalin315@gmail.com>
2023-06-11 20:48:12 +0300

    решение 1 и 2 задач (сем. 6)

---
+++ hwsem6/zadacha1s6/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+int[] CreateMassiv(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+
+    return array;
+}
+void PrintMas(int[] array)
+{
+    Console.WriteLine("Ваш массив");
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine("]");
+    //Console.WriteLine();
+}
+int SearchNumber(int[] array)
+{
+    int positivnum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            positivnum++;
+        }
+    }
+    Console.WriteLine($"Кол-во положительных элементов массива = {positivnum}");
+    return positivnum;
+}
+int size = Prompt("Введите длину массива: ");
+int[] mas = CreateMassiv(size);
+PrintMas(mas);
+SearchNumber(mas);
---
+++ hwsem6/zadacha2s6/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, 
+// которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+int Prompt (string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    if ((int.TryParse(value, out int val)) == false)
+    {
+        Console.WriteLine("Это не число ");
+    }
+    return val;
+}
+int c1=Prompt("Введите значение коэфициента b1 в формуле 'y = k1 * x + b1' : ");
+int a1=Prompt("Введите значение коэфициента k1 в формуле 'y = k1 * x + b1' : ");
+double b1=-1;
+int c2=Prompt("Введите значение коэфициента b2 в формуле 'y = k1 * x + b1' : ");
+int a2=Prompt("Введите значение коэфициента k2 в формуле 'y = k1 * x + b1' : ");
+double b2=-1;
+double crossDotX = (((b1*c2)-(b2*c1)) / ((a1*b2)-(a2*b1)));
+double crossDotY = (((c1*a2)-(c2*a1)) / ((a1*b2)-(a2*b1)));
+Console.WriteLine ($"Точка пересечения заданных прямых (x,y): ( {crossDotX} , {crossDotX} )");
