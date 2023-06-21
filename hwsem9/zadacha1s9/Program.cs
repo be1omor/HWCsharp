cf3b18aba7aba5cffddd4b26bfd26e2ee5becb99
Ilya Lyalin <lyalin315@gmail.com>
2023-06-21 15:42:38 +0300

    решение 1й задачи(сем. 9)

---
+++ hwsem9/zadacha1s9/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая 
+// выведет все натуральные числа в 
+// промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+int Promt(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+void NaturalNumber(int number, int count)
+{
+    if (number >= count)
+    {
+        Console.Write($"{number} ");
+        NaturalNumber(number - 1, count);
+    }
+}
+
+// string PrintNumber(int number)
+// {
+//     if (number == 1) return "1";
+
+//     return $"{number} {PrintNumber(number - 1)}";
+// }
+
+int n = Promt("Введите число: ");
+int i = 1;
+//Console.WriteLine(PrintNumber(n));
+NaturalNumber(n, i);
