8a2856c0faaca6b24b4d0e74306cef4c844b6919
Ilya Lyalin <lyalin315@gmail.com>
2023-06-21 17:18:48 +0300

    решение 2й задачи(сем. 9)

---
+++ hwsem9/zadacha2s9/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+int Promt(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int SumNaturalNum(int num1, int num2, int num3)
+{
+    num3 += num1;
+    if (num1 != num2)
+    {
+        return SumNaturalNum(num1 + 1, num2, num3);
+    }
+    return num3;
+}
+
+int m = Promt("Введите число M: ");
+int n = Promt("Введите число N: ");
+int z = 0;
+Console.Write($"Сумма натуральных элементов в промежутке от M до N = {SumNaturalNum(m, n, z)}");
